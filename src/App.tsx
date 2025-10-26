2127d250ff6d9071b119d3b380789dee8a46a6ed
Renaldas-Nds <renaldas.jakas@netdrifters.com>
2025-10-26 15:50:10 +0100

    add Notfound page and styling, correct About.tsx file name to follow convention

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import Layout from './components/layout'
+import Layout from './components/Layout'
 import Home from './pages/Home'
 import About from './pages/About'
 import NotFound from './pages/NotFound'
